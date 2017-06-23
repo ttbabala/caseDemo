1a4361ccb8032ebb776486437ec756a97d3bd11b
ttbabala <hqiotzx@126.com>
2017-06-24 00:07:25 +0800

    1st

---
+++ class.php
@@ -0,0 +1,37 @@
+<?php
+	class base{
+		private $id = 0;
+		private $name = '';
+		private $pw = '';
+		public function __construct($id=0,$name=null,$pw=null){
+			$this -> id= $id;
+			$this -> name = $name;
+			$this -> pw = $pw;
+		}
+		
+		public function __get($proName){
+			return $this -> $proName;
+		}
+		
+		public function __set($proName,$proValue){
+			$this -> $proName = $proValue;
+		}
+		
+		public function bcryptHash($rowpw,$rand=8){
+			if($rand < 4 || $rand > 31) $rand=8;
+			$salt = '$2a$'.str_pad($rand,2,'0',STR_PAD_LEFT).'$';
+			$randomValue = openssl_random_pseudo_bytes(16);
+			$salt.= substr(strtr(base64_encode($randomValue),'+','.'),0,22);
+			return crypt($rowpw,$salt);		
+		}
+		public function bcryptVerfy($rowpw,$storedHash){
+			return crypt($rowpw,$storedHash) == $storedHash;
+		}
+		public function getinfo(){
+			$rowpw = $this -> pw;
+			$hashpw = $this -> bcryptHash($rowpw);
+			echo "my id is ".$this -> id.";"." my name is ".$this -> name."; my password is ".$hashpw.";";
+			
+		}
+	}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	require_once('class.php');
+	$person = new base(2,'zhangsan','zs888');
+	$person -> getinfo();
+?>
